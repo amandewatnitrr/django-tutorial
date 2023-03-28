b6e777f511babb208a0a92138932af09ba337060
amandewatnitrr <amandewatnitrr@gmail.com>
2023-03-28 16:24:45 +0530

    Added Content - Models and Admin Panel | Adding Models and Tables to the Database - Implemented as Well

--- test_project/projects/admin.py
+++ test_project/projects/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Project
 # Register your models here.
+
+admin.site.register(Project) 
+# This will register the Model Project and show it on the admin panel.
--- test_project/projects/models.py
+++ test_project/projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True) 
+    # Setting the null=true means that it's not nessecary to have a description, it's an optional thing.
+    # null by default is always set to false.
+    # blank=True again means this field can be empty.
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    created =  models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
