6cdedf5e3c8c82cee6eb4610edd9c126a84f1ef1
amandewatnitrr <amandewatnitrr@gmail.com>
2023-09-19 15:15:47 +0530

    Added Content and Implemented - Mailing System on SignUp - Completed

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -119,6 +119,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 # You can find it by googling "gmail PORT"
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djangoproject296@gmail.com'
+EMAIL_HOST_PASSWORD = 'lityopearxagdlvw'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- test_project/users/signals.py
+++ test_project/users/signals.py
@@ -3,12 +3,25 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
         profile = Profile.objects.create(user=user, username=user.username, email=user.email, name=user.first_name)
+
+        subject = "Welcome to Devsearch"
+        message = "Thanks for Siging Up :D"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
     print("Profile Saved Succesfully... ")
 
 def updateProfile(sender, instance, created, **kwargs):
