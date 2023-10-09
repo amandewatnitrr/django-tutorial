7a8bc45c40113ad2087e711b4200f1f9a4256060
amandewatnitrr <amandewatnitrr@gmail.com>
2023-10-09 08:00:55 +0530

    Authentication with JSON Web Tokens | Implemented, executed and Tested

--- test_project/api/views.py
+++ test_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from projects.models import Project
 from .serializers import ProjectSerializer
@@ -21,6 +22,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProjects(request):
+    print("User: ",request.user)
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
     return Response(serializer.data)
